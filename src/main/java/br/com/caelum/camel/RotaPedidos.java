19040503a441f0fffdc04c9760a07bbc2ff2ca8c
igoryy <igor.siviero1@gmail.com>
2020-12-04 00:48:09 -0300

    Melhor legibilidade rotas

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -18,6 +18,13 @@ public class RotaPedidos {
 			@Override
 			public void configure() throws Exception {
 				from("file:pedidos?delay=5s&noop=true")
+				
+				.multicast()
+					.to("direct:http")
+					.to("direct:soap");
+					
+					from("direct:http")
+					.routeId("rota-http")
 					.setProperty("pedidoId", xpath("/pedido/id/text()"))
 					.setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).split()
 						
@@ -28,6 +35,13 @@ public class RotaPedidos {
 						.setHeader(Exchange.HTTP_QUERY, simple(
 								"ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}"))
 						.to("http4://localhost:8080/webservices/ebook/item");
+			
+			
+				from("direct:soap")
+					.routeId("rota-soap")
+					.setBody(constant("<envelope> teste </envelope>"))
+					.log("${body}")
+				.to("mock:soap");
 			}
 		});
 
