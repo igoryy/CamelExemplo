22c2d4bc9d76299b890efa16426e4877e95412fa
igoryy <igor.siviero1@gmail.com>
2020-12-03 21:12:04 -0300

    integrar via http ao webservice

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,8 +1,10 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -23,9 +25,8 @@ public class RotaPedidos {
 					.filter().xpath("/item/formato[text()='EBOOK']")
 				.log("${id}")
 				.marshal().xmljson()
-				.log("${body}")
-				.setHeader("CamelFileName", simple("${file:name.noext}.json"))
-				.to("file:saida");
+				.setHeader(Exchange.HTTP_METHOD, HttpMethods.GET)
+				.to("http4://localhost:8080/webservices/ebook/item");
 			}
 		});
 		
